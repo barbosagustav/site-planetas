2caeebdbb41e478d8fbd1beeef83fcac9937668e
Gustavo Barbosa <gbarbosagustavo01@gmail.com>
2023-07-13 16:57:19 -0300

    Iniciando o projeto

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Header from'./components/Header'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <h1>Teste</h1>
+    </>
+  )
+}
+
+export default App
---
+++ components/Header/index.jsx
@@ -0,0 +1,10 @@
+import './header.css'
+
+const Header = () => {
+    return (<>
+    <h1>Teste Header</h1>
+    </>
+    )
+}
+
+export default Header
